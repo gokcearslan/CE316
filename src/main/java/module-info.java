40995e8403bc53d1d682f854301d33df57a27984
2rall7 <haciyevtural124@gmail.com>
2023-05-16 01:01:25 +0400

    Final commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
-module com.example.ce316team5project {
+module com.example.ce316project10may {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires java.desktop;
 
 
-    opens com.example.ce316team5project to javafx.fxml;
-    exports com.example.ce316team5project;
+    opens com.example.ce316project10may to javafx.fxml;
+    exports com.example.ce316project10may;
 }
